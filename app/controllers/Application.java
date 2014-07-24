d08f399e0ffe36e7825e2800cff17aabb7b022b4
Joao Trindade <trindade.joao@gmail.com>
2014-07-24 10:54:44 +0100

    Populating db

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -26,8 +26,25 @@ public class Application extends Controller {
         return redirect(routes.Application.index());
     }
 
+    public static void addRingtonePrivate(String name, String uri, int price) {
+        Ringtone ring =   new Ringtone();
+        ring.setName(name);
+        ring.setUri(uri);
+        ring.setPrice(price);
+        ring.save();
+    }
+
     public static Result getRingtones() {
         List<Ringtone> rings = new Model.Finder(String.class, Ringtone.class).all();
+        if(rings.size() == 0) {
+            addRingtonePrivate("Sumol", "http://www.hipersuper.pt/wp-content/uploads/2011/11/sumol.jpg", 123);
+            addRingtonePrivate("Coca-Cola", "http://fandbnews.com/wp-content/uploads/2014/02/Coca-Cola-Logo.jpg", 231);
+            addRingtonePrivate("Clube Joanense", "http://gallery.jhwebdesigner.com/wp-content/uploads/2012/08/mount_sinai_soccer_club_logo_300dpi_png_file.jpg", 93);
+            addRingtonePrivate("Zara", "http://img4.wikia.nocookie.net/__cb20121226055648/logopedia/images/5/53/Zara-LOGO-blackwhite.jpg", 81);
+            addRingtonePrivate("BCP", "https://gmkfreelogos.com/logos/M/img/Millennium_bcp-1.gif", 88);
+            addRingtonePrivate("CGD", "http://images.all-free-download.com/images/graphiclarge/cgd_2_62577.jpg", 19);
+            rings = new Model.Finder(String.class, Ringtone.class).all();
+        }
         return ok(Json.toJson(rings));
     }
 
