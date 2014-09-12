e09cb4134210309f67da26a6b598e391260cf77e
Joao Trindade <trindade.joao@gmail.com>
2014-09-12 18:21:59 +0100

    New db

--- populate_db.sql
+++ populate_db.sql
@@ -1,8 +1,14 @@
 insert into event VALUES (1, 2, 0, 1, 10);
-insert into thing VALUES (1, "Sumol", "http://www.hipersuper.pt/wp-content/uploads/2011/11/sumol.jpg", 123);
-insert into thing VALUES (2, "Coca-Cola", "http://fandbnews.com/wp-content/uploads/2014/02/Coca-Cola-Logo.jpg", 231);
-insert into thing VALUES (3, "A club", "http://gallery.jhwebdesigner.com/wp-content/uploads/2012/08/mount_sinai_soccer_club_logo_300dpi_png_file.jpg", 93);
-insert into thing VALUES (4, "Zara", "http://img4.wikia.nocookie.net/__cb20121226055648/logopedia/images/5/53/Zara-LOGO-blackwhite.jpg", 81);
-insert into thing VALUES (5, "BCP", "http://4vector.com/thumb_data/afd-33897.jpg", 88);
-insert into thing VALUES (6, "CGD", "http://images.all-free-download.com/images/graphiclarge/cgd_2_62577.jpg", 19);
+insert into thing VALUES (1, "Letra", "http://www.meiosepublicidade.pt/wp-content/uploads/2014/01/letra.jpg", 123);
+insert into thing VALUES (2, "Frubis", "http://www.meiosepublicidade.pt/wp-content/uploads/2014/01/frubis.png", 231);
+insert into thing VALUES (3, "Tété", "http://tete.pt/imagens/produtos/m3.jpg", 93);
+insert into thing VALUES (4, "Casa Grande", "http://www.meiosepublicidade.pt/wp-content/uploads/2014/01/ALTA-300x213.jpg", 81);
+insert into thing VALUES (5, "Antiga Barbearia de Bairro", "http://www.100ml.pt/abb/imagens/logo_abb.png", 88);
+insert into thing VALUES (6, "Boa Boca", "http://boaboca-gourmet.com/wp/wp-content/uploads/2011/11/bboca_bisccanela1.jpg", 19);
+insert into thing VALUES (7, "Bolas de Praia", "http://www.meiosepublicidade.pt/wp-content/uploads/2014/01/bolas-de-praia-300x200.jpg", 19);
+insert into thing VALUES (8, "FrutaFormas", "https://www.frutaformas.pt/images/produtos/croca_maca.jpg", 19);
+insert into thing VALUES (9, "José Gourmet", "http://erinspens.files.wordpress.com/2011/12/jose-packaging.jpg", 19);
+insert into thing VALUES (10, "Meia-Dúzia", "http://www.winespiritus.com/906-1352-thickbox/extra-jam-meia-duzia-pack-6x75gr-extra-jam.jpg", 19);
+insert into thing VALUES (11, "Pingunça", "http://static.squarespace.com/static/5151aed5e4b0030300310dbf/515b73b7e4b03ae0a10d07be/515b73cee4b0daad6e73a758/1364997939577/pingu%C3%A7a-flavours.jpg?format=200w", 19);
+insert into thing VALUES (12, "Yonest", "http://www.yonest.pt/img/yonest_logo.png", 19);
 
