7a3b288fe30f287889fe1e124644484fca664b70
Joao Trindade <trindade.joao@gmail.com>
2014-09-18 21:53:41 +0100

    Finishing touches to script

--- convert_cvs2sql.py
+++ convert_cvs2sql.py
@@ -1,12 +1,14 @@
 import csv
 import pdb
+import sys
 
 from PIL import Image         # python-imaging
 import urllib
 
-def create_sql(seq, name, category, text, homepage, url_logo, url_product):
+def create_sql(f, seq, name, category, text, homepage, url_logo, url_product):
 
-    print name
+    sys.stderr.write(name + '\n')
+
     urllib.urlretrieve(url_logo, "logo.jpg")
     urllib.urlretrieve(url_product, "product.jpg")
 
@@ -16,14 +18,19 @@ def create_sql(seq, name, category, text, homepage, url_logo, url_product):
     im = Image.open("product.jpg")
     productX, productY = im.size
 
-    sql = 'insert into thing VALUES ( %d, "%s", "%s", "%s", "%s", "%s", "%s", %d, %d, %d, %d);' \
+    sql = 'insert into thing VALUES ( %d, "%s", "%s", "%s", "%s", "%s", "%s", %d, %d, %d, %d);\n' \
             % (seq, name, text, category, homepage, url_logo, url_product, logoX, logoY, productX, productY)
-    print sql
+
+    f.write(sql)
 
 with open('companies.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     reader.next()                                   # Skip the first line
     seq = 1
+
+    f = open('populate_db.sql', 'w')
     for name, category, text, homepage, url_logo, url_product in reader:
-        create_sql(seq, name.strip(), category.strip(), homepage.strip(), text.strip(), url_logo.strip(), url_product.strip())
+        create_sql(f, seq, name.strip(), category.strip(), homepage.strip(), text.strip(), url_logo.strip(), url_product.strip())
         seq += 1
+
+    f.close()
